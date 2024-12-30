bdd46ef503d24e607d12e26e596bff584bafe829
Alexandrentt <156978899+Alexandrentt@users.noreply.github.com>
2024-12-30 14:04:36 -0600

    Update Main.java
    Originaly write in Spanish, now only in English.

--- Main.java
+++ Main.java
@@ -2,36 +2,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese a: ");
+        System.out.println("a: ");
         double a = sc.nextDouble();
-        System.out.println("Ingrese b: ");
+        System.out.println("b: ");
         double b = sc.nextDouble();
-        System.out.println("Ingrese c: ");
+        System.out.println("c: ");
         double c = sc.nextDouble();
         if (a == 0){
             if (b == 0){
-                System.out.println("La ecuación no tiene solución");
+                System.out.println("The equation has no solution");
             } else {
                 double x = -c / b;
-                System.out.println("La solución es: " + x);    
+                System.out.println("The solution is: " + x);    
             }
-        }else{
+        } else {
             double discri = (b*b) - (4*a*c);
-            System.out.println(">> El discriminante es "+discri);
-            if (discri>= 0){
+            System.out.println(">> The discriminant is " + discri);
+            if (discri >= 0){
                 double x1 = (-b + Math.sqrt(discri)) / (2*a);
                 double x2 = (-b - Math.sqrt(discri)) / (2*a);
-                System.out.println("Las soluciones son: " + x1 + " y " + x2);
-            } else{
-                double parteReal = -b / (2 * a);
-                double parteImaginaria = Math.sqrt(-discri) / (2 * a);
+                System.out.println("The solutions are: " + x1 + " and " + x2);
+            } else {
+                double realPart = -b / (2 * a);
+                double imaginaryPart = Math.sqrt(-discri) / (2 * a);
     
-                System.out.println("Las soluciones son imaginarias y son:");
-                System.out.println(parteReal + " + " + parteImaginaria + "i");
-                System.out.println(parteReal + " - " + parteImaginaria + "i");
+                System.out.println("The solutions are imaginary and are:");
+                System.out.println(realPart + " + " + imaginaryPart + "i");
+                System.out.println(realPart + " - " + imaginaryPart + "i");
             }
         }
         
         sc.close();
     }
 }
+
